b7f3073152c782b0ea5ca79b6010f69741b28a08
Ming Zhao <dmwwmd123321@163.com>
2017-05-05 11:24:42 +0800

    Initial commit

---
+++ src/com/test/Main.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Main {
+    Integer as = 2;
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        System.out.println(m.as);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
